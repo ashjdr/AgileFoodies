f021bc245dc54001151100c2397972cb28d216c7
s6068159 <juli-simons@hotmail.com>
2019-02-14 12:07:12 +0000

    Main with button launch to timer activity now functional Added class with splash screen adn changed colour scheme new launcher icon implemented

---
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package marshmallow.cis2003.tees.ac.uk.agilefoodies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
