780ecab82b606b6eb393677f24d6b918170da67b
t7062534 <jackwaines97@googlemail.com>
2019-02-28 12:48:04 +0000

    adding a home page

---
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/homePage.java
@@ -0,0 +1,10 @@
+package marshmallow.cis2003.tees.ac.uk.agilefoodies;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by t7062534 on 28/02/19.
+ */
+
+public class homePage extends AppCompatActivity {
+}
