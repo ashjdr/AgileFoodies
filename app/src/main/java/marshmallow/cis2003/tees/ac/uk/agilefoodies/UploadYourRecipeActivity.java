c85f6c785bbc7673d121d79fbe6e98f11bb0de89
s6068159 <juli-simons@hotmail.com>
2019-03-05 14:14:33 +0000

    added image picker logic

--- app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/UploadYourRecipeActivity.java
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/UploadYourRecipeActivity.java
@@ -1,23 +1,78 @@
 package marshmallow.cis2003.tees.ac.uk.agilefoodies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View.OnClickListener;
+
 
 public class UploadYourRecipeActivity extends AppCompatActivity {
-    private Button addImage;
+    private ImageButton addImage;
+    private Button saveUplaod;
     private EditText edittext;
     private ImageView addimage;
+
+        private final int SELECT_PHOTO = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_your_recipe);
-        addImage  = findViewById(R.id.button_set);
+
+
         edittext  = findViewById(R.id.edit_text1);
-        addimage  = findViewById(R.id.imageView);
+        addimage  = findViewById(R.id.uploadedImage);
+
+            addImage = findViewById(R.id.imageButtonAddImage);
+            addImage.setOnClickListener(new OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                    photoPickerIntent.setType("image/*");
+                    startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+                }
+            });
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+            super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+            switch(requestCode) {
+                case SELECT_PHOTO:
+                    if(resultCode == RESULT_OK){
+                        try {
+                            final Uri imageUri = imageReturnedIntent.getData();
+                            final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                            addimage.setImageBitmap(selectedImage);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+            }
+        }
 
     }
-}
+
+
+
+
+
+
+
+
+
