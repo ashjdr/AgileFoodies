967d1564044c1f330150ef23e9b42a1a95dec61f
s6068159 <juli-simons@hotmail.com>
2019-03-05 12:48:15 +0000

    Added imageview, image button and edit text to activity_upload_your_recipe

--- app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/UploadYourRecipeActivity.java
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/UploadYourRecipeActivity.java
@@ -2,12 +2,23 @@ package marshmallow.cis2003.tees.ac.uk.agilefoodies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class UploadYourRecipeActivity extends AppCompatActivity {
+    private Button addImage;
+    private EditText edittext;
+    private ImageView addimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe);
+        setContentView(R.layout.activity_upload_your_recipe);
+        addImage  = findViewById(R.id.button_set);
+        edittext  = findViewById(R.id.edit_text1);
+        addimage  = findViewById(R.id.imageView);
+
+
     }
 }
