36ec9bec3543d71ac293cfa6e828b1d01e0eeb44
s6068159 <juli-simons@hotmail.com>
2019-03-06 10:52:35 +0000

    some refactoring and changes to select/take photo

--- app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/UploadYourRecipeActivity.java
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/UploadYourRecipeActivity.java
@@ -29,15 +29,13 @@ import android.view.View.OnClickListener;
 public class UploadYourRecipeActivity extends AppCompatActivity {
     private ImageButton addImage;
     private ImageButton takePhoto;
-    private ImageButton saveUplaod;
+    private ImageButton saveUpload;
     private EditText edittext;
-    private ImageView addimage;
-    private String currentPhotoPath;
-    private ContactsContract.Data data;
+    private ImageView uploadedImage;
 
 
-    private final int SELECT_PHOTO = 1;
-        private final int REQUEST_TAKE_PHOTO = 2;
+
+
 
 
 
@@ -47,98 +45,56 @@ public class UploadYourRecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_upload_your_recipe);
 
 
-        edittext  = findViewById(R.id.edit_text1);
-        addimage  = findViewById(R.id.uploadedImage);
+        edittext = findViewById(R.id.edit_text1);
+        uploadedImage = findViewById(R.id.uploadedImage);
         takePhoto = findViewById(R.id.takePhoto);
+        addImage = findViewById(R.id.imageButtonAddImage);
 
-            addImage = findViewById(R.id.imageButtonAddImage);
-            addImage.setOnClickListener(new OnClickListener() {
 
-                @Override
-                public void onClick(View view) {
-                    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                    photoPickerIntent.setType("image/*");
-                    startActivityForResult(photoPickerIntent, SELECT_PHOTO);
-                }
-            });
+        addImage.setOnClickListener(new OnClickListener() {
 
-            takePhoto = findViewById(R.id.takePhoto);
-            takePhoto.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        @Override
-        public void onClick(View view) {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                // Create the File where the photo should go
-                File photoFile = null;
-                try {
-                    photoFile = createImageFile();
-                } catch (IOException ex) {
-
-                }
-                // Continue only if the File was successfully created
-                if (photoFile != null) {
-                    Uri photoURI;
-                    photoURI = FileProvider.getUriForFile(this,
-                            "com.example.android.fileprovider",
-                            photoFile);
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-                }
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto, 1);
             }
-        }
+        });
 
-    });
-}
-
-
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-            super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-
-            switch(requestCode) {
-                case SELECT_PHOTO:
-                    if(resultCode == RESULT_OK){
-                        try {
-                            final Uri imageUri = imageReturnedIntent.getData();
-                            final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                            addimage.setImageBitmap(selectedImage);
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-
-                        }
-                    }
-                case REQUEST_TAKE_PHOTO:{
-                    if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-                        Bundle extras = data.();
-                        Bitmap imageBitmap = (Bitmap) extras.get("data");
-                        addimage.setImageBitmap(imageBitmap);
-                    }
-                }
-
-
-                }
-
+        takePhoto.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePicture, 0);
             }
-        }
+        });
 
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch(requestCode) {
+            case 0:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    uploadedImage.setImageURI(selectedImage);
+                }
+
+                break;
+            case 1:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    uploadedImage.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }}
+
+
+
+
+
 
 
 
