18f6ce25c17076d4c11f366227e0ecf5e00343e4
s6068159 <juli-simons@hotmail.com>
2019-03-19 14:38:58 +0000

    Changed recipe icon

--- app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/RecipeFragment.java
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/RecipeFragment.java
@@ -1,7 +1,5 @@
 package marshmallow.cis2003.tees.ac.uk.agilefoodies;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -10,33 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.support.annotation.NonNull;
-import android.util.Log;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.firebase.firestore.SetOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FileDownloadTask;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-
-import java.io.File;
-import java.io.IOException;
 
 import static android.content.ContentValues.TAG;
 
@@ -78,27 +56,7 @@ public class RecipeFragment extends Fragment {
                 }
             }
         });
-//    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-//    final DatabaseReference ref = database.getReference().child("jack/0/ingredients/0/name");
-//
-//
-//    ref.addListenerForSingleValueEvent(new ValueEventListener() {
-//        @Override
-//        public void onDataChange(DataSnapshot dataSnapshot) {
-//            String value = (String) dataSnapshot.getValue();
-//            recipetext.append(value);
-//
-//
-//        }
-//
-//
-//        @Override
-//        public void onCancelled(DatabaseError databaseError) {
-//
-//        }
-//
-//
-//    });
+
 
 
         return v;
