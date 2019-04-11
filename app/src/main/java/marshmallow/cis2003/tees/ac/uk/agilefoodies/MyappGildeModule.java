8c2c8d425499906ed1191adb7da4c20e8095be98
jackwaines <jackwaines97@googlemail.com>
2019-04-11 15:00:16 +0100

    not finished can't find a way to get image from fire storgae om.firebaseui:firebase-ui-storage:???

--- app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/MyappGildeModule.java
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/MyappGildeModule.java
@@ -20,6 +20,7 @@ public class MyAppGlideModule extends AppGlideModule {
     @Override
     public void registerComponents(Context context, Glide glide, Registry registry) {
         // Register FirebaseImageLoader to handle StorageReference
-        registry.append(StorageReference.class, InputStream.class, new FirebaseImageLoader.Factory());
+        registry.append(StorageReference.class, InputStream.class, new FirebaseImageLoader.Factory()
+        );
     }
 }
