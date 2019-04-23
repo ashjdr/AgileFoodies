5cbdfd0bf0cd91d272e3e3d43aa9e38e8d865af0
s6068159 <juli-simons@hotmail.com>
2019-04-23 14:03:34 +0100

    TescoLabs works on click of ingredients timer starts on click of time name search works ingredient search still in progress changes to layout

--- app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/RecipeFragment.java
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/RecipeFragment.java
@@ -1,8 +1,6 @@
 package marshmallow.cis2003.tees.ac.uk.agilefoodies;
-import android.app.VoiceInteractor;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -11,23 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+
 import java.util.List;
 import java.util.Timer;
+
 import static android.content.ContentValues.TAG;
 
-public class RecipeFragment extends Fragment implements SearchRecipeFragment.OnFragmentInteractionListener  {
+public class
+RecipeFragment extends Fragment implements SearchRecipeFragment.OnFragmentInteractionListener  {
     public TextView recipetext;
     public TextView nameView;
     public TextView timeView;
@@ -38,6 +37,7 @@ public class RecipeFragment extends Fragment implements SearchRecipeFragment.OnF
     String mTextEntered;
     String mQueryType;
     DocumentReference docRef;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -58,9 +58,81 @@ public class RecipeFragment extends Fragment implements SearchRecipeFragment.OnF
         nameView = v.findViewById(R.id.recipe_name);
         database = FirebaseFirestore.getInstance();
         CollectionReference recipes = database.collection("recipes");
-        if (mQueryType == "recipeName"){
-        docRef = database.collection(recipes.getId()).document("" + mTextEntered);}
-        else if (mQueryType == "ingredientName"){Log.d(TAG, "ingredient search");}
+        if (mQueryType == "ingredientName") {
+
+           Query query =  recipes.whereEqualTo("ingredientName", mTextEntered);
+
+
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    LinearLayout ingredient = getActivity().findViewById(R.id.ingredient);
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            StringBuilder fields = new StringBuilder ("");
+                            fields.append("\nVegan?: ").append(document.get("Vegan"));
+                            fields.append("\nVegetarian?: ").append(document.get("Vegetarian"));
+                            fields.append("\nCategory: ").append(document.get("category"));
+
+
+                            List<String> group = (List<String>) document.get("ingredients");
+                            TextView tView = new TextView(getContext());
+                            if (group == null){
+                                tView.setText("No ingredients");
+                            }
+                            else {
+                                tView.append("Ingredients:\n");
+                                for  (final String element: group){
+                                    tView.append(element + "\n");
+                                    tView.setOnClickListener(new View.OnClickListener() {
+                                        public void onClick(View v) {
+                                            Intent intent = new Intent(RecipeFragment.this.getActivity(), tescoLab.class);
+                                            intent.putExtra("ingredient", element);
+                                            startActivity(intent);
+                                        }
+                                    });
+                                }
+                                ingredient.addView(tView);
+                                //fields.append("\n" + element);
+                            }
+                            final long timing = (long)document.get("time");
+
+                            timeView.setText("Time: "+ timing) ;
+                            final String name = (String)document.get("name");
+                            nameView.setText("Recipe Name: "+ name) ;
+                            timeView.setOnClickListener(new View.OnClickListener(){
+                                public void onClick(View v){
+                                    mListener.onFragmentInteraction(timing, name);
+                                }
+
+
+                            });
+
+                            fields.append("\n\nInstructions:");
+                            List<String> group2 = (List<String>) document.get("instructions");
+                            if (group2 == null){
+                                fields.append("\n No instructions found");}
+                            else{
+                                int num = 1;
+                                for (String element: group2){
+                                    fields.append("\n" + num + ") " + element + "\n" );
+                                    num++;
+                                }
+                                recipetext.setText(fields);}
+                        } else {
+                            Log.d(TAG, "No such document");
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });}
+
+
+        else if (mQueryType == "recipeName"){
+        docRef = database.collection(recipes.getId()).document("" + mTextEntered);
+
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -80,10 +152,9 @@ public class RecipeFragment extends Fragment implements SearchRecipeFragment.OnF
                             tView.setText("No ingredients");
                         }
                         else {
-                        for (final String element: group){
-
-                                tView.setText(element);
-                                ingredient.addView(tView);
+                            tView.append("Ingredients:\n");
+                        for  (final String element: group){
+                                tView.append(element + "\n");
                                 tView.setOnClickListener(new View.OnClickListener() {
                                     public void onClick(View v) {
                                         Intent intent = new Intent(RecipeFragment.this.getActivity(), tescoLab.class);
@@ -92,9 +163,10 @@ public class RecipeFragment extends Fragment implements SearchRecipeFragment.OnF
                                     }
                                 });
                             }
+                            ingredient.addView(tView);
                             //fields.append("\n" + element);
                         }
-                        final long timing = (long)document.get("Time");
+                        final long timing = (long)document.get("time");
 
                         timeView.setText("Time: "+ timing) ;
                         final String name = (String)document.get("name");
@@ -125,7 +197,7 @@ public class RecipeFragment extends Fragment implements SearchRecipeFragment.OnF
                     Log.d(TAG, "get failed with ", task.getException());
                 }
             }
-        });
+        });}
         return v;
     }
 
