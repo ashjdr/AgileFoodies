353fd1d1fe6523995cabcace037250b6a21a2e9c
Juli Simons <juli-simons@hotmail.com>
2019-04-24 23:38:37 +0100

    Changed some colours in nearest shop buttons

--- app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/FindNearestShopFragment.java
+++ app/src/main/java/marshmallow/cis2003/tees/ac/uk/agilefoodies/FindNearestShopFragment.java
@@ -2,6 +2,7 @@ package marshmallow.cis2003.tees.ac.uk.agilefoodies;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,12 +10,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FindNearestShopFragment extends Fragment {
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_find_nearest_shop, container, false);
+
+
+        TextView V1 = v.findViewById(R.id.textView3);
+        V1.setTextSize(20);
+        V1.setTypeface(V1.getTypeface(), Typeface.BOLD);
+
 
         Button B1 = v.findViewById(R.id.tescoButton);
         B1.setOnClickListener(new View.OnClickListener() {
